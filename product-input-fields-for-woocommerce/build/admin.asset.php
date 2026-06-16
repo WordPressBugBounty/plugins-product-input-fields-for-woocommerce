d3c596869b1831313f821554fb584261e29fa464
Sean Murphy <sean@iamseanmurphy.com>
2026-06-16 14:51:57 +0000

    2.0.1

---
+++ product-input-fields-for-woocommerce/build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-components/build-style/style.css', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c29cbba3f887ac00b241');
---
+++ product-input-fields-for-woocommerce/build/product.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-components/build-style/style.css', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '44b2c5d8376ce08bc1d4');
